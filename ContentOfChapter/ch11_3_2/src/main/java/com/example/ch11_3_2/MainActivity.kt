3f1abdc21652abcfde1b33eb4b44013517f6161e
ka <insideka01@gmail.com>
2021-12-21 20:54:30 +0900

    12.21 ch11 11-2 appcompat 라이브러리 - 액션바의 메뉴 구성 - onCreateOptionsMenu(), onOptionsItemSelected()

---
+++ ContentOfChapter/ch11_3_2/src/main/java/com/example/ch11_3_2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ch11_3_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuItem1: MenuItem? = menu?.add(0, 0, 0, "menu1")
+        val menuItem2: MenuItem? = menu?.add(0, 1, 0, "menu2")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        0 -> {
+            Log.d("kkang", "menu1 click")
+            true
+        }
+        1 -> {
+            Log.d("kkang", "menu2 click")
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+}

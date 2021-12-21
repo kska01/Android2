b00def2c198ba25867356d085a9154f5c0d94cc4
ka <insideka01@gmail.com>
2021-12-21 23:59:08 +0900

    12.21 ch11 11-2 appcompat 라이브러리 - 액션 뷰 이용

--- ContentOfChapter/ch11_3_4/src/main/java/com/example/ch11_3_4/MainActivity.kt
+++ ContentOfChapter/ch11_3_4/src/main/java/com/example/ch11_3_4/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.ch11_3_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(p0: String?): Boolean {
+                //검색어 변경 이벤트
+                return true
+            }
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+                return true
+            }
+        })
+        return true
     }
 }

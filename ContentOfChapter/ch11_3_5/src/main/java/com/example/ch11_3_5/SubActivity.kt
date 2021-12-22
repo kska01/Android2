50418122f2ffd8b3d719d2c55ae5c6b5a0200273
ka <insideka01@gmail.com>
2021-12-22 21:55:46 +0900

    12.22 ch11 actionbar ~ toolbar + up button in toolbar

--- ContentOfChapter/ch11_3_5/src/main/java/com/example/ch11_3_5/SubActivity.kt
+++ ContentOfChapter/ch11_3_5/src/main/java/com/example/ch11_3_5/SubActivity.kt
@@ -3,6 +3,7 @@ package com.example.ch11_3_5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.example.ch11_3_5.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
@@ -12,15 +13,21 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         setSupportActionBar(binding.toolbar)
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+// 아래 주석 코드는 있어도 없어도 일단 실행에는 문제가 없으나 아직 정확하게 기능을 모름 더 공부 필요
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val id = item.itemId
+//        when (id) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
+//            }
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)

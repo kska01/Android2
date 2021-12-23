41f15001911438275d9d2e4419838992989aa3cf
ka <insideka01@gmail.com>
2021-12-23 11:03:59 +0900

    12.23 fragment

---
+++ ContentOfChapter/ch11_3_6/src/main/java/com/example/ch11_3_6/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ch11_3_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.ch11_3_6.databinding.FragmentOneBinding
+
+// 프래그먼트 구현
+class OneFragment : Fragment() {
+    lateinit var binding: FragmentOneBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 프래그먼트 동 제어
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = OneFragment()
+        transaction.add(R.id.fragment_content, fragment)
+        transaction.commit()
+    }
+}

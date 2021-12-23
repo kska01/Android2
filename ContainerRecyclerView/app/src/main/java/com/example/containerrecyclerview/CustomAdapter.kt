8a55780d1e5e397c61e3711dfd6a2c7b4bd8b573
ka <insideka01@gmail.com>
2021-12-24 01:15:54 +0900

    12.24 RecyclerView - 생성까지

---
+++ ContainerRecyclerView/app/src/main/java/com/example/containerrecyclerview/CustomAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.containerrecyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.containerrecyclerview.databinding.ItemRecyclerBinding
+import java.text.SimpleDateFormat
+
+class CustomAdapter: RecyclerView.Adapter<Holder>() {
+    var listData = mutableListOf<Memo>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val memo = listData.get(position)
+        holder.setMemo(memo)
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+}
+
+class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+    fun setMemo(memo: Memo) {
+        binding.textNo.text = "${memo.no}"
+        binding.textTitle.text = memo.title
+        var sdf = SimpleDateFormat("yyyy/MM/dd")
+        var formattedDate = sdf.format(memo.timestamp)
+        binding.textDate.text = formattedDate
+    }
+}
